b1b52a9d101629d037971e6c75ea8dbc62850dc9
Michael Sweet <msweet37@gmail.com>
2019-10-23 11:42:22 -0400

    reset menu addition

---
+++ Assets/Scripts/Startup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public class Startup
+{
+    static Startup()
+    {
+        Debug.Log("Up and running");
+        //EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        //EditorSceneManager.OpenScene("Assets/Starter Scene.unity");
+
+        //EditorSceneManager.OpenScene(Application.dataPath + "Assets/Starter Scene.unity");
+        //EditorUtility.LoadWindowLayout("Default");
+    }
+}
