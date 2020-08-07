dda170292bf106c5c09002023230b4b68dc749a6
Michael Sweet <msweet37@gmail.com>
2020-08-07 07:28:21 -0700

    Update MusicSnapshotSwitch.cs

--- Assets/Scripts/MusicSnapshotSwitch.cs
+++ Assets/Scripts/MusicSnapshotSwitch.cs
@@ -2,26 +2,55 @@
 using System.Collections;
 using UnityEngine.Audio;
 
-public class MusicSnapshotSwitch : MonoBehaviour {
+public class MusicSnapshotSwitch : MonoBehaviour
+{
 
 	public AudioMixerSnapshot mySnapshot;
 	public float fadeTime = 3.0f;
 	public float delayTime = 0.0f;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start()
+	{
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update()
+	{
+
 	}
 
-	void OnTriggerEnter () {
-		mySnapshot.TransitionTo (fadeTime);
+	void OnTriggerEnter()
+	{
+		Debug.Log("OnTriggerEnter");
+		if (delayTime < 0.05f)
+		{
+			mySnapshot.TransitionTo(fadeTime);
+		}
+		else
+		{
+			StartCoroutine(startDelayedTransition());
+
+		}
 
 
+
+
+	}
+
+	IEnumerator startDelayedTransition()
+	{
+		//Print the time of when the function is first called.
+		Debug.Log("Delayed snapshot switch starting in: " + delayTime + " seconds.  The current time is: " + Time.time);
+
+		//yield on a new YieldInstruction that waits for 5 seconds.
+		yield return new WaitForSeconds(delayTime);
+
+		//After we have waited 5 seconds print the time again.
+		Debug.Log("Beginning delayed snapshot at: " + Time.time);
+
+		mySnapshot.TransitionTo(fadeTime);
 	}
 
 }
